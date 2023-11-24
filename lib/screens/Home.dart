8b2fa2a8b3c3cb6fc5229a6c36871023ae0c8aa1
Willian_Pedro_Santin <willian_p_santin@estudante.sc.senai.br>
2023-11-24 23:56:35 +0000

    home

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../components/Header.dart';
 import '../components/Menu.dart';
@@ -14,10 +13,36 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: Header(context),
-        drawer: Menu(context),
-        body: Center(
-          child: Text("Welcome!"),
-        ));
+      appBar: Header(context),
+      drawer: Menu(context),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://boletimjuridico.publicacoesonline.com.br/wp-content/uploads/2015/10/estacionamento.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(
+                16.0), // Adicione um preenchimento ao redor do texto
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                  10.0), // Adicione bordas arredondadas se desejar
+              color: Colors.black.withOpacity(0.5), // Cor de fundo do retângulo
+            ),
+            child: Text(
+              "Welcome!",
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
